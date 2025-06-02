7596c0d1af71c726240161ebf08de305770eb13d
Ayushi-Rai-20 <arai8548@gmail.com>
2025-06-02 18:09:34 +0530

    functions

--- 2-control-structures/solution.py
+++ 2-control-structures/solution.py
@@ -7,6 +7,9 @@ else:
     print(f"{num} is not divisible by 2")
 
 
+# ------------------------------------------------------------------------------------------------------------------------------------------
+
+
 # 2. Print ticket prices based on the age of the customer. If a child is between ages 0-12 - print "$5". Teenagers between ages 13-17 - print "$8", Adults between ages 18-64 print "$12" and Seniors between ages 65 and above print "$7".
 age = int(input("Enter your age: "))
 match age:
---
+++ 4-functions/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+te):
+#     total_cost = sum(prices)
+#     tax = (total_cost * tax_rate) / 100
+#     total_bill = total_cost + tax
+#     return total_bill
+
+
+# prices = [10.99, 4.99, 2.99]
+# tax_rate = 8
+# total_amount = round(calculate_total_bill(prices, tax_rate), 2)
+# print('Total amount is ', total_amount)
